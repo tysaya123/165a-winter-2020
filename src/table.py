6d971dea81cffaa7cf02007e2e145ffe1858db9f
Kamal Sadek <kamaljalals@gmail.com>
2020-02-01 15:49:22 -0800

    Fill out select and insert in table.py
    Filled out some of the comments, dependent on page.py right now, so some
    of the function calls might need to be updated when the API is
    finalized.

--- src/table.py
+++ src/table.py
@@ -1,6 +1,3 @@
-from template.page import *
-from time import time
-
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
 TIMESTAMP_COLUMN = 2
@@ -17,9 +14,11 @@ class Record:
 class Table:
 
     """
-    :param name: string         #Table name
-    :param num_columns: int     #Number of Columns: all columns are integer
-    :param key: int             #Index of table key in columns
+    :param name: string             #Table name
+    :param num_columns: int         #Number of Columns: all columns are integer
+    :param key: int                 #Index of table key in columns
+    :param index: int               #???
+    :param bufferpool: BufferPool   #Reference to the global bufferpool
     """
     def __init__(self, name, num_columns, key, index, bufferpool):
         self.name = name
@@ -28,6 +27,7 @@ class Table:
         self.page_directory = {}
         self.index = index
         self.bufferpool = bufferpool
+        self.indirection = {}
 
         # A reference to the current base pages and the tail page
         self.base_pages = [None] * num_columns
@@ -41,10 +41,13 @@ class Table:
         rid = rid_counter
         rid_counter += 1
 
-        for base_page in base_pages:
+        for index, base_page in enumerate(base_pages):
+
+            # Check to see if base page for column has space. If not, allocate
+            # new page, and update references to it.
             if not base_page.has_capacity():
-                # Allocate another base page
-                # Update references to this new page
+                new_base_page = bufferpool.new_base_page()
+                self.base_pages[index] = new_base_page
 
             # Write to the base page
             base_page.write(rid, columns)
@@ -56,13 +59,26 @@ class Table:
         rid = index.get(column[0])
         pids = page_directory[rid]
 
+        # Result of the select
+        vals = []
+
+        # First, check to see if any of the columns have the dirty bit set
+        # for this particular record.
         has_dirty_bit = False
         for pid in pids:
-            # If dirty bit, set has_dirty_bit to True, break
-            pass
+            page = bufferpool.get(pid)
+            if page.has_dirty():
+                has_dirty_bit = True
+                break
+            vals.append(page.read(rid))
 
+        # If record has a dirty bit, pull the tail page, and get the values
+        # from there. If not, then pull the values from the base pages.
         if has_dirty_bit:
-            # Pull tail page
+            tail_page = page_directory[indirection[rid]]
+            vals = tail_page.read(rid)
+
+        return vals
 
 
     def delete(self, key):
@@ -83,6 +99,7 @@ class Table:
             # page.write(/* */)
 
         if not tail_page.has_capacity():
+            pass
             # Allocate new tail page, update references
 
         # Write to tail page
