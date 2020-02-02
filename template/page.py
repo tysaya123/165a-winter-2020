ee9f89e2fbd589824b2123f38a420945c5576b15
Tycho Yacub <tychoyacub@gmail.com>
2020-02-01 16:17:55 -0800

    Fixed page for python3 update should not work.

--- template/page.py
+++ template/page.py
@@ -1,16 +1,84 @@
+import logging
+import struct
+
 from template.config import *
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+# https://stackoverflow.com/questions/9940859/fastest-way-to-pack-a-list-of-floats-into-bytes-in-python
 
 
 class Page:
-
     def __init__(self):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.data = bytearray(PAGE_SIZE)
+        self.record_size = None
+        self.record_format = None
+        self.max_records = None
 
     def has_capacity(self):
-        pass
+        if self.num_records < self.max_records:
+            return True
+        else:
+            return False
 
-    def write(self, value):
+    def get_max(self): return self.max_records
+
+    def read(self, rid):
+        for i in range(self.num_records):
+            curr_record = self.get_record(i)
+            if curr_record[0] == rid:
+                return curr_record
+        return None
+
+    def append(self, data):
+        self.set_record(self.num_records, data)
         self.num_records += 1
-        pass
 
+    def update(self, rid, data, offset=0, length=0):
+        if length == 0: length = self.record_size
+
+        for i in range(self.num_records):
+            curr_record = self.get_record(i)
+            if curr_record[0] == rid:
+                self.data[i * self.record_size + offset:i * self.record_size + offset + length] = data
+        return None
+
+    def get_record(self, i):
+        return struct.unpack(self.record_format,
+                             self.data[i * self.record_size:i * self.record_size + self.record_size])
+
+    def set_record(self, i, data):
+        self.data[i * self.record_size:i * self.record_size + self.record_size] = data
+
+
+class BasePage(Page):
+    def __init__(self):
+        super().__init__()
+        self.record_size = RID_SIZE + VALUE_SIZE + SCHEMA_SIZE
+        self.record_format = ENDIAN_FORMAT + RID_FORMAT + VALUES_FORMAT + SCHEMA_FORMAT
+        self.max_records = int(PAGE_SIZE / self.record_size)
+
+    def new_record(self, rid, value, dirty):
+        logging.debug(str(self.num_records) + "/" + str(self.max_records))
+        # logging.debug(self.record_format + ":" + str(rid) + ":" + str(value) + ":" + str(dirty))
+        record_data = struct.pack(self.record_format, rid, value, dirty)
+        self.append(record_data)
+
+    def set_dirty(self, rid, dirty):
+        dirty = struct.pack(ENDIAN_FORMAT + SCHEMA_FORMAT, dirty)
+        self.update(rid, dirty, RID_SIZE + VALUE_SIZE, SCHEMA_SIZE)
+
+
+class TailPage(Page):
+    def __init__(self, num_cols):
+        super().__init__()
+        self.num_cols = num_cols
+        self.record_size = RID_SIZE + num_cols * VALUE_SIZE
+        self.record_format = ENDIAN_FORMAT + RID_FORMAT + num_cols * VALUES_FORMAT
+        self.max_records = int(PAGE_SIZE / self.record_size)
+
+    def new_record(self, rid, values):
+        record_data = struct.pack(self.record_format, rid, *values)
+        self.append(record_data)
--- template/test.py
+++ template/test.py
@@ -1,35 +1,50 @@
 import unittest
+import logging
 from unittest import TestCase
+from random import randrange
 
-from page import BasePage
+
+from page import BasePage, TailPage
 from config import *
 
 
-class TestBasePageMethods(TestCase):
+class TestPageMethods(TestCase):
     def setUp(self):
-        self.test_page = BasePage()
+        self.base_page = BasePage()
+        self.num_tail_cols = 5
+        self.tail_page = TailPage(self.num_tail_cols)
+        self.max_int = 9223372036854775807  # 2^63 - 1
+        self.max_rid = 4294967295  # 2^32
+        self.min_int = -self.max_int
+        self.min_rid = 0
 
-    def testHasCapacity(self):
-        self.assertTrue(self.test_page.has_capacity())
+    def testMaxMinValues(self):
+        self.base_page.new_record(self.max_rid, self.max_int, 0)
+        tail_values = self.num_tail_cols * [self.max_int]
+        self.tail_page.new_record(self.max_rid, tail_values)
 
-    def testReadWrite(self):
-        rand_int = 242444
-        rand_rid = 44
-        max_int = 9223372036854775807  # 2^63 - 1
-        max_rid = 4294967295  # 2^32
-        min_int = -max_int
-        min_rid = 0
-        self.test_page.write(rand_rid, rand_int)
-        self.test_page.write(max_rid, max_int)
-        self.test_page.write(min_rid, min_int)
+        self.assertEqual((self.max_rid, self.max_int, 0), self.base_page.read(self.max_rid))
+        self.assertEqual(tuple(tail_values), self.tail_page.read(self.max_rid)[1:self.num_tail_cols+1])
 
-        self.assertEqual(self.test_page.read(rand_rid), rand_int)
-        self.assertEqual(self.test_page.read(max_rid), max_int)
-        self.assertEqual(self.test_page.read(min_rid), min_int)
+        self.base_page.new_record(self.min_rid, self.min_int, 0)
+        tail_values = self.num_tail_cols * [self.min_int]
+        self.tail_page.new_record(self.min_rid, tail_values)
 
-        self.assertEqual(self.test_page.getDirty(rand_rid), SchemaEncoding.CLEAN)
-        self.assertEqual(self.test_page.getDirty(max_rid), SchemaEncoding.CLEAN)
-        self.assertEqual(self.test_page.getDirty(min_rid), SchemaEncoding.CLEAN)
+        self.assertEqual((self.max_rid, self.max_int, 0), self.base_page.read(self.max_rid))
+        self.assertEqual(tuple(tail_values), self.tail_page.read(self.min_rid)[1:self.num_tail_cols+1])
+
+    def testBaseCapacity(self):
+        for i in range(self.base_page.get_max()):
+            logging.debug(i)
+            self.assertEqual(True, self.base_page.has_capacity())
+            self.base_page.new_record(randrange(self.max_rid+1), randrange(self.min_int, self.max_int+1), randrange(2))
+        self.assertEqual(False, self.base_page.has_capacity())
+
+    def testTailCapacity(self):
+        for i in range(self.tail_page.get_max()):
+            self.assertEqual(True, self.tail_page.has_capacity())
+            self.tail_page.new_record(randrange(self.max_rid+1), 5*[randrange(self.min_int, self.max_int+1)])
+        self.assertEqual(False, self.tail_page.has_capacity())
 
 
 if __name__ == '__main__':
