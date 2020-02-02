d5bedc8a32b36799d1e22da644f6e050167f7d72
Tycho Yacub <tychoyacub@gmail.com>
2020-02-01 17:13:03 -0800

    Added mark_as_deleted to the page class

--- src/test.py
+++ src/test.py
@@ -34,7 +34,6 @@ class TestPageMethods(TestCase):
 
     def testBaseCapacity(self):
         for i in range(self.base_page.get_max()):
-            logging.debug(i)
             self.assertEqual(True, self.base_page.has_capacity())
             self.base_page.new_record(randrange(self.max_rid+1), randrange(self.min_int, self.max_int+1), randrange(2))
         self.assertEqual(False, self.base_page.has_capacity())
@@ -45,6 +44,16 @@ class TestPageMethods(TestCase):
             self.tail_page.new_record(randrange(self.max_rid+1), 5*[randrange(self.min_int, self.max_int+1)])
         self.assertEqual(False, self.tail_page.has_capacity())
 
+    def testDeleteRecord(self):
+        self.base_page.new_record(self.max_rid, self.max_int, 0)
+        tail_values = self.num_tail_cols * [self.max_int]
+        self.tail_page.new_record(self.max_rid, tail_values)
+
+        self.base_page.mark_record_deleted(self.max_rid)
+        self.tail_page.mark_record_deleted(self.max_rid)
+
+        self.assertEqual(None, self.base_page.read(self.max_rid))
+        self.assertEqual(None, self.tail_page.read(self.max_rid))
 
 if __name__ == '__main__':
     unittest.main()
