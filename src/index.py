4f7356482995893ce483ee86a47ed78e574028f3
Kamal Sadek <kamaljalals@gmail.com>
2020-02-21 18:18:11 -0800

    Update index, table for secondary keys, add tests
    Fixed bugs related to index.
    
    Added functionality for secondary keys for all table functions. Added tests that made sure this functionality works.

--- src/index.py
+++ src/index.py
@@ -1,31 +1,32 @@
-from table import Table
-
 class Index:
 
-    def __init__(self, table):
+    def __init__(self):
         #Maps key to rid
         self.index = {}
     """
     # get function finds key in index dictionary, returns list of rids
     """
     def get(self, key):
-        return self.index[key]
+        return self.index.get(key)
 
     """
         # set function appends the rid to a list of rids from given key
     """
     def set(self, key, rid):
+        if not self.index.get(key):
+            self.index[key] = []
         self.index[key].append(rid)
 
     """
         # delete function finds given key and removes using pop function
     """
-    def delete(self, key):
-        for i in self.index[key]:
-            if i == key:
-                self.index.pop(key)
+    def delete(self, key, value):
+        arr = self.index[key]
+        for i in reversed(range(len(arr))):
+            if arr[i] == value:
+                arr.pop(i)
 
-"""
+    """
     def locate(self, value):
 
         pass
