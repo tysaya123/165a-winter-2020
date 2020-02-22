e9a9ccbcf8d1a2688ca1247972a3206ec5ebbd21
doodlez510 <doodlez510@gmail.com>
2020-02-21 17:00:29 -0800

    implemented the new index get,set,delete functions

--- src/index.py
+++ src/index.py
@@ -1,33 +1,31 @@
 from table import Table
 
-"""
-# optional: Indexes the specified column of the specified table to speed up select queries
-# This data structure is usually a B-Tree
-"""
-
-
 class Index:
 
     def __init__(self, table):
         #Maps key to rid
         self.index = {}
-
-        pass
-
     """
-    # returns the location of all records with the given value
+    # get function finds key in index dictionary, returns list of rids
     """
-
     def get(self, key):
         return self.index[key]
 
+    """
+        # set function appends the rid to a list of rids from given key
+    """
     def set(self, key, rid):
-        self.index[key]=rid
+        self.index[key].append(rid)
 
-    def delete(self , key):
-        del self.index[key]
+    """
+        # delete function finds given key and removes using pop function
+    """
+    def delete(self, key):
+        for i in self.index[key]
+            if i == key
+                self.index.pop(key)
 
-
+"""
     def locate(self, value):
 
         pass
@@ -46,3 +44,4 @@ class Index:
 
     def drop_index(self, table, column_number):
         pass
+"""
