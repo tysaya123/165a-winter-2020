3aab0f604780c5752db4572133fd9462a2021a42
Tycho Yacub <tychoyacub@gmail.com>
2020-02-25 12:16:06 -0800

    initial saving of db to disk

--- src/db.py
+++ src/db.py
@@ -1,19 +1,56 @@
 from table import Table
 from bufferpool import BufferPool
+from os import listdir, path, mkdir
+import pickle
+import glob
 
 class Database():
 
     def __init__(self):
         self.tables = {}
-        self.bufferpool = BufferPool()
+        self.bufferpool = None
+        self.folder = None
 
-    def open(self, file):
-        #TODO pass file to bufferpool
-        pass
+    def open(self, folder):
+        self.folder = path.expanduser(folder)
+        if not path.isdir(self.folder):
+            mkdir(self.folder, 0o777)
+            self.bufferpool = BufferPool(self.folder)
+            return
+        self.open_bufferpool()
+        self.open_tables()
+
+    def open_bufferpool(self):
+        file = self.folder + 'bufferpool.pkl'
+        with open( file, 'r' ) as f:
+            bufferpool = BufferPool(self.folder)
+            pkl = pickle.load(f)
+            bufferpool.dump(pkl)
+            self.bufferpool = bufferpool
+
+    def open_tables(self):
+        files = glob(self.folder + '*_table.pkl')
+        for file in files:
+            with open(folder + '/' + file, 'r') as f:
+                table = Table(self.bufferpool)
+                pkl = pickle.load(f)
+                table.dump(pkl)
+                self.tables[table.name] = table
 
     def close(self):
-        self.bufferpool.close_file()
+        self.close_bufferpool()
+        self.close_tables()
 
+    def close_bufferpool(self):
+        with open(self.folder + '/' + 'bufferpool.pkl', 'wb') as f:
+            pkl = self.bufferpool.dump()
+            f.write(pkl)
+
+    def close_tables(self):
+        for name, table in self.tables.items():
+            with open(self.folder + '/' + name + '_table.pkl', 'wb') as f:
+                pkl = table.dump()
+                f.write(pkl)
     """
     # Creates a new table
     :param name: string         #Table name
@@ -22,6 +59,7 @@ class Database():
     """
     def create_table(self, name, num_columns, key):
         table = Table(name, num_columns, key, self.bufferpool)
+
         self.tables[name] = table
         return table
 
